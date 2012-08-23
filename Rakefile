d5ef285357d51295dd1f1901b9159914820ec49c
Ezekiel Templin <zeke@templ.in>
2012-08-23 18:14:58 +0100

    Update spec setup

--- Rakefile
+++ Rakefile
@@ -3,10 +3,4 @@ require 'rspec/core/rake_task'
 
 Bundler::GemHelper.install_tasks
 
-desc 'Run all specs in spec directory'
-RSpec::Core::RakeTask.new(:spec) do |spec|
-  spec.rspec_opts = %w(-fd -c)
-  spec.pattern = 'spec/**/*_spec.rb'
-end
-
 task :default => :spec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'rspec'
+require File.expand_path(File.dirname(__FILE__) + '/../lib/fassbinder')
+require 'vcr'
 
-require File.expand_path('../../lib/fassbinder', __FILE__)
+VCR.configuration do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :typhoeus
+  config.configure_rspec_meta_data!
+  config.default_cassette_options = { :record => :new_episodes }
+end
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.run_all_when_everything_filtered = true
+  c.filter_run :focus
+end
